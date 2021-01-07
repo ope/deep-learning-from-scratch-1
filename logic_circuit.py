b590317eaa3e09bd885ab91918d20f9f3b30fc4a
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-01-07 21:23:51 +0900

    Added XOR

--- logic_circuit.py
+++ logic_circuit.py
@@ -30,7 +30,11 @@ def OR(x1, x2):
     else:
         return 1
 
-
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
 
 
 if __name__ == '__main__':
@@ -43,3 +47,6 @@ if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
         y = OR(xs[0], xs[1])
         print("OR" + str(xs) + " -> " + str(y))
+    for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = XOR(xs[0], xs[1])
+        print("XOR" + str(xs) + " -> " + str(y))        
