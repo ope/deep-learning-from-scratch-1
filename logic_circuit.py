e0808e03bb6ef30d9fed580fa0350ec4aeb36025
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-01-12 20:55:45 +0900

    Changed softmax funxtion against overflow

--- logic_circuit.py
+++ logic_circuit.py
@@ -71,7 +71,8 @@ def forward(network, x):
     return y
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
